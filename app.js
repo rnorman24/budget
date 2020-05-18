499ca2e2a844f45d34af5e51aa48c49aee79dea5
unknown <nray24@gmail.com>
2020-05-18 10:24:21 -0600

    add controller structure

---
+++ app.js
@@ -0,0 +1,14 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+  // Some code
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+  // Some code
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function() {
+  // Some code
+})(budgetController, UIController);
