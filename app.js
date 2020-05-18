e3e2b5ec0f59dfb96d25715248076f7e984a0cd7
unknown <nray24@gmail.com>
2020-05-18 10:30:34 -0600

    add controller structure

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ var UIController = (function() {
 var controller = (function() {
   
   document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log('button was clicked.');
+    
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+    
   });
 
 })(budgetController, UIController);
