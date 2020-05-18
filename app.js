328e344df9ef0140e3fb243e6e80cb0421b48903
unknown <nray24@gmail.com>
2020-05-18 10:45:25 -0600

    add controller structure

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 // BUDGET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
+
   // Some code
+
 })();
 
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
+
   // Some code
+
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function() {
+var controller = (function () {
+
+  var ctrlAddItem = function () {
   
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    
     // 1. Get the field input data
-
+  
     // 2. Add the item to the budget controller
-
+  
     // 3. Add the item to the UI
-
+  
     // 4. Calculate the budget
-
+  
     // 5. Display the budget on the UI
+  
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function () {
+
+
 
   });
 
-  document.addEventListener('keypress', function(event) {
+  document.addEventListener("keypress", function (event) {
 
     if (event.keyCode === 13 || event.which === 13) {
-      console.log('ENTER was pressed.');
+
+      console.log("ENTER was pressed.");
+    
     }
 
-  })
-
+  });
+  
 })(budgetController, UIController);
-
-
