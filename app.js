f602183e33612e7a62ef62514a6107fe631069ce
unknown <nray24@gmail.com>
2020-05-18 11:34:22 -0600

    add controller structure

--- app.js
+++ app.js
@@ -8,21 +8,34 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
   return {
     getInput: function() {
       return {
-        type: document.querySelector('.add__type').value, // Will be either inc or exp
-        decription: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
-      }
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        decription: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
       
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
     }
-  }
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function () {
   
@@ -40,7 +53,7 @@ var controller = (function (budgetCtrl, UICtrl) {
   
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (event) {
 
