d1850ccbf6be7fd11c7ef151f737ca45e3973c30
unknown <nray24@gmail.com>
2020-05-19 09:11:37 -0600

    add init function

--- app.js
+++ app.js
@@ -29,20 +29,28 @@ var UIController = (function () {
       return DOMstrings;
     }
   };
-
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
 
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();    
+      }
+    });
+  };
+  
   var ctrlAddItem = function () {
   
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
-  
+    
     // 2. Add the item to the budget controller
   
     // 3. Add the item to the UI
@@ -53,14 +61,13 @@ var controller = (function (budgetCtrl, UICtrl) {
   
   };
 
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-  document.addEventListener("keypress", function (event) {
-
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();    
+  return {
+    init: function() {
+      console.log('Application has started.');
+      setupEventListeners();
     }
-
-  });
+  }
 
 })(budgetController, UIController);
+
+controller.init();
