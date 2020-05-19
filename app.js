c9ad0e08d01e9c0709e36a89aadaf5e7820a7a7d
unknown <nray24@gmail.com>
2020-05-19 11:08:15 -0600

    add data structure

--- app.js
+++ app.js
@@ -33,8 +33,11 @@ var budgetController = (function () {
       // ID = last ID + 1
 
       // Create new ID
-      ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
       // Create new item based on 'inc' or 'exp' type
       if (type === 'exp') {
         newItem = new Expense(ID, des, val);
@@ -48,6 +51,10 @@ var budgetController = (function () {
       // Return the new element
       return newItem;
       
+    },
+
+    testing: function() {
+      console.log(data);
     }
   };
 
@@ -96,11 +103,13 @@ var controller = (function (budgetCtrl, UICtrl) {
   };
   
   var ctrlAddItem = function () {
+    var input, newItem;
   
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
     
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
   
     // 3. Add the item to the UI
   
