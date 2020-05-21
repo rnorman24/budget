ce5fcf9a38cf5f88ba8c034a43a4323f68f1ac96
unknown <nray24@gmail.com>
2020-05-21 09:41:31 -0600

    add new item to ui

--- app.js
+++ app.js
@@ -68,7 +68,9 @@ var UIController = (function () {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -82,19 +84,19 @@ var UIController = (function () {
     },
 
     addListItem: function(obj, type) {
-      var html;
+      var html, newHtml;
       // Create HTML string with placeholder text
 
       if (type === 'inc') {
         html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === 'exp') {
         html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-      }
-      
-
-      
+      }      
 
       // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = html.replace('%description%', obj.description);
+      newHtml = html.replace('%value%', obj.value);
 
       // Insert the HTML into the DOM
 
