56393484d33c16fdb156748ea705fef109d2b7e5
unknown <nray24@gmail.com>
2020-05-22 09:44:56 -0600

    add clearing input fields

--- app.js
+++ app.js
@@ -152,10 +152,13 @@ var controller = (function (budgetCtrl, UICtrl) {
   
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+
+    // 4. Clear the input fields
+    UICtrl.clearFields();
   
-    // 4. Calculate the budget
+    // 5. Calculate the budget
   
-    // 5. Display the budget on the UI
+    // 6. Display the budget on the UI
   
   };
 
