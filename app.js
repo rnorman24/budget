ca69cde0d5af16ef896be872452183a73112fa42
unknown <nray24@gmail.com>
2020-05-26 13:04:23 -0600

    add item delete to budget controller

--- app.js
+++ app.js
@@ -177,6 +177,11 @@ var UIController = (function () {
 
     },
 
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID)
+      el.parentNode.removeChild(el);
+    },
+
     clearFields: function() {
       var fields, fieldsArr;
 
@@ -276,10 +281,12 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       // 1. Delete the item from the data structure
       budgetCtrl.deleteItem(type, ID);
+
       // 2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
 
       // 3. Update and show the new budget
-
+      updateBudget();
     }
   };
 
