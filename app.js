9ffe24fe9c110f97a786a74dac22ea6a00aa69d1
unknown <nray24@gmail.com>
2020-05-26 10:29:40 -0600

    add event delete listener using event delegation

--- app.js
+++ app.js
@@ -82,6 +82,10 @@ var budgetController = (function () {
       });
 
       index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
     },
 
     calculateBudget: function() {
