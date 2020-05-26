eb0109ffe21304b78fcb1c382c8c8778174b59ea
unknown <nray24@gmail.com>
2020-05-26 09:07:54 -0600

    add budget to ui

--- app.js
+++ app.js
@@ -117,7 +117,11 @@ var UIController = (function () {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   return {
@@ -166,7 +170,13 @@ var UIController = (function () {
       });
       // Set cursor back to desciption field
       fieldsArr[0].focus();
+    },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
     },
 
     getDOMstrings: function() {
