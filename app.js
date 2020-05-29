cc0a88e34c0df2b7053cd61605327a85e3a68d93
unknown <nray24@gmail.com>
2020-05-29 11:40:53 -0600

    add number formatting

--- app.js
+++ app.js
@@ -264,8 +264,8 @@ var UIController = (function () {
       obj.budget > 0 ? type = 'inc' : type === 'exp';
 
       document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, );
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+      document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
       
       if (obj.percentage > 0) {
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
